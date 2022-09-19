504ec0f49bbcf26593652600c562945244d2a4d4
Patrick Swiggers <patrick.swiggers@gmail.com>
2022-09-19 20:07:31 +0200

    first get endpoint

---
+++ customers/models.py
@@ -0,0 +1,6 @@
+from re import M
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    industry = models.CharField(max_length=100)
---
+++ customers/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+from customers.models import Customer
+from customers.serializers import CustomerSerializer
+
+def customers(request):
+    # invoke serializer and return new client
+    data = Customer.objects.all()
+    serializer = CustomerSerializer(data, many = True)
+    return JsonResponse({'customers': serializer.data})
